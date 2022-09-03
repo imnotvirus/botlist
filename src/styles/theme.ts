c7b81bca9a0bdc02e022f87f7e8e5e9f6bb93dc0
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-09-03 17:27:42 -0300

    theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,10 @@ export default {
 	COLORS: {
 		primary: '#1a2437',
 		background: '#f5f8f9',
-		text: '#56616e',
-		textLight: '#607b99',
+		text: '#56616E',
+		text_200: '#52636C',
+		text_100: '#738192',
+		text_50: '#607b99',
+		shape: '#fff',
 	},
 };
